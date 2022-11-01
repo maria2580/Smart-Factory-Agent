74824906b94270ce0dba8c680c22fb68f8248143
just_user <marin6670@gmail.com>
2022-11-01 09:05:40 +0900

    이름과 명령어를 저장하는 센서 객체 생성

---
+++ src/main/java/org/primitive/SensorRelates/Sensor.java
@@ -0,0 +1,37 @@
+package org.primitive.SensorRelates;
+
+public class Sensor {
+    private String name;
+    private String command="";
+
+    public Sensor(String name, String command) {
+        this.name = name;
+        this.command = command;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+    public String getValue(){
+        Cmd cmd = new Cmd();
+
+        String command = cmd.inputCommand(this.command);
+        String result = cmd.execCommand(command);
+
+        System.out.println(result);
+        return "";
+    }
+
+}
