b0b74ea222692191d5920c127520557e67fc66e1
just_user <marin6670@gmail.com>
2022-11-05 18:13:51 +0900

    생성자 호출시 센서 객체 파일 읽어 들이는 함수 호출하여 필드에 저장

--- src/main/java/org/primitive/SensorRelates/Sensors.java
+++ src/main/java/org/primitive/SensorRelates/Sensors.java
@@ -1,15 +1,19 @@
 package org.primitive.SensorRelates;
 
+import org.primitive.Main;
+
 import java.util.ArrayList;
 
 public class Sensors {
-    private ArrayList<Sensor> sensors;
-    Sensors(){
+    public ArrayList<Sensor> sensors;
+    public Sensors(){
+        sensors= Main.read_Sensor_object();
         //Todo 파일 읽기 시도, 없으면 빈 파일 생성 있으면 열어서 센서 객체에 담기
     }
     public SensorValue[] getAllSensorValue(){
         SensorValue[] sensorValues=new SensorValue[sensors.size()];
         for(int i=0;i<sensors.size();i++){
+            sensorValues[i]=new SensorValue();
             sensorValues[i].name=sensors.get(i).getName();
             sensorValues[i].value=sensors.get(i).getValue();
         }
