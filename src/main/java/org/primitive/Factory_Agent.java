2c16e1564bff60a7e90f86c046e9a51c0bc52d74
just_user <marin6670@gmail.com>
2022-11-18 12:54:23 +0900

    센서값 읽기 테스트 완료 센서 리스트 조회 함수 Sensors로 이동

--- src/main/java/org/primitive/Factory_Agent.java
+++ src/main/java/org/primitive/Factory_Agent.java
@@ -15,25 +15,28 @@ public class Factory_Agent {
         this.loginToken= loginToken;
         this.ID=ID;
         sensors=new Sensors();
-        System.out.println("Factory agent 생성. sensors 내용:");
         //Todo sensors 멤버변수에 파일 읽기로 저장된 센서 객체파일(이름,명령어 리스트 객체파일) 읽기로 초기화
     }
     public void execute(){
-        Thread thread=new Thread(){
-            @Override
-            public void run() {
-                super.run();
-                try {
-                    Thread.sleep(10000);
-                } catch (InterruptedException e) {e.printStackTrace();}
-                SensorValue[] sensorValues= sensors.getAllSensorValue();
-                for (int i=0;i<sensorValues.length;i++) {
-                    System.out.println(sensorValues[i].name+" : "+sensorValues[i].value);
+//        Thread thread=new Thread(){
+//            @Override
+//            public void run() {
+//                super.run();
+                while (true) {
+                    SensorValue[] sensorValues = sensors.getAllSensorValue();
+                    for (int i = 0; i < sensorValues.length; i++) {
+                        System.out.println(sensorValues[i].name + " : " + sensorValues[i].value);
+                    }
+                    //Callretrofit.post_sensors_data(sensors.getAllSensorValue(),ID,loginToken);
+                    try {
+                        Thread.sleep(10000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
-                //Callretrofit.post_sensors_data(sensors.getAllSensorValue(),ID,loginToken);
-            }
-        };
-        thread.start();
+//            }
+//        };
+//        thread.start();
 
     }
 
