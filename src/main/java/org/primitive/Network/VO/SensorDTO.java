f7a7e7a50d860370410849d4e52a85dea5aec9dc
just_user <marin6670@gmail.com>
2022-11-25 14:16:17 +0900

    @Body 매개변수는 함수당 하나만 있어야 하므로 DTO,VO 객체로 만들어서 전달

---
+++ src/main/java/org/primitive/Network/VO/SensorDTO.java
@@ -0,0 +1,12 @@
+package org.primitive.Network.VO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class SensorDTO {
+    private String name;
+    private String command;
+    private String user_id;
+}
---
+++ src/main/java/org/primitive/Network/VO/SensorValueDTO.java
@@ -0,0 +1,14 @@
+package org.primitive.Network.VO;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.primitive.SensorRelates.SensorValue;
+@AllArgsConstructor
+@Getter
+public class SensorValueDTO {
+    private SensorValue[] sensorValues;
+    private String ID;
+    private String loginToken;
+}
