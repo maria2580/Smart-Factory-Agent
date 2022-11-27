2067e6ee141243f4119d40b54c442e4f6709ed0f
just_user <marin6670@gmail.com>
2022-11-28 04:43:11 +0900

    버그 수정, 센서 객체는 공장객체의 필드로 합쳐서 사용

--- src/main/java/org/primitive/Main.java
+++ src/main/java/org/primitive/Main.java
@@ -9,7 +9,7 @@ import java.util.*;
 
 public class Main {
     public static String user_id;
-    public static Sensors sensors;
+    public static Factory_Agent factory_agent;
     public static void main(String[] args) {
 
         /*//파일 입출력 테스트 코드
@@ -41,7 +41,6 @@ public class Main {
         System.out.println("\n\nFactory Agnet program\nPrimitive_number_one\n");
         //check login or register
         LoginToken loginToken = new LoginToken("initiated");
-        String ID="default";
         boolean flag=false;
         do {
             System.out.println("1. 로그인 2. 회원등록");
@@ -53,35 +52,32 @@ public class Main {
                     System.out.print("PW: ");
                     String input_PW = new Scanner(System.in).next();
                     loginToken = new LoginToken(Callretrofit.post_login_request(input_ID,input_PW));
-                    if (loginToken.getTokenValue().equals(LoginStatus.DENIED)) {
+                    if (loginToken.getTokenValue().equals(LoginStatus.DENIED+"")) {
                         System.out.println("로그인 실패, id와 비밀번호를 다시 확인해 주세요");
                     }
-                    else if(loginToken.getTokenValue().equals(LoginStatus.PERMITTED)) {
+                    else if(loginToken.getTokenValue().equals(LoginStatus.PERMITTED+"")) {
                         user_id=input_ID;
                         flag=true;
                     }
                     break;
                 case "2":
                     System.out.println("회원등록을 시작합니다. ID와 PW를 입력하세요.");
-                    do {
-                        System.out.print("ID: ");
-                        String input_ID1 = new Scanner(System.in).next();
-                        ID=input_ID1;
-                        System.out.print("PW: ");
-                        String input_PW1 = new Scanner(System.in).next();
-                        loginToken = new LoginToken(Callretrofit.post_signUp_request(input_ID1, input_PW1));
-                        if (loginToken.getTokenValue().equals(LoginStatus.EXIST_ID)) {
-                            System.out.println("sign up 실패. 같은 유저가 있습니다. 다시 입력하세요.\n");
-                        }
-                        if (loginToken.getTokenValue().equals(LoginStatus.PERMITTED)) {
-                            user_id=input_ID1;
-                            flag=true;
-                        }
-                    } while (loginToken.getTokenValue().equals("initiated")||loginToken.getTokenValue().equals("exist user"));
+                    System.out.print("ID: ");
+                    String input_ID1 = new Scanner(System.in).next();
+                    System.out.print("PW: ");
+                    String input_PW1 = new Scanner(System.in).next();
+                    loginToken = new LoginToken(Callretrofit.post_signUp_request(input_ID1, input_PW1));
+                    if (loginToken.getTokenValue().equals(LoginStatus.EXIST_ID+"")) {
+                        System.out.println("sign up 실패. 같은 유저가 있습니다. 다시 입력하세요.\n");
+                    }
+                    if (loginToken.getTokenValue().equals(LoginStatus.PERMITTED+"")) {
+                        user_id=input_ID1;
+                        flag=true;
+                    }
                     break;
             }
-        }while (flag==true);
-        sensors=new Sensors(user_id);//센서값 읽어오기;
+        }while (!flag);
+        factory_agent = new Factory_Agent(loginToken,user_id);//공장 객체 생성, 내부에 센서배열 객체 있음
         do{
             clearConsole();//Todo 콘솔창 왜 안지워질까
             System.out.println("\n1. Agent 실행\n2. 환경 설정\n");
@@ -89,7 +85,6 @@ public class Main {
             String input = new Scanner(System.in).next();
             switch (input){
                 case "1":
-                    Factory_Agent factory_agent = new Factory_Agent(loginToken,ID);
                     factory_agent.execute();
                     break;
                 case "2":
@@ -113,7 +108,7 @@ public class Main {
             case "2":
                 //추가인지 수정인지를 먼저 선택하게 한뒤
                 //두 경우 모두, 파일 없으면 생성, 있으면 열어서 전부 불러오기.
-
+                factory_agent.sensors.print();
                 System.out.println("1.추가 2. 수정 3. 삭제" );
                 String input3=new Scanner(System.in).next();
                 String name;
@@ -121,34 +116,42 @@ public class Main {
                 switch (input3){
 
                     case "1":
-                        System.out.println("센서명을 입력하세요");
+                        System.out.println("센서명을 입력하세요(skip: quit)");
                         name = new Scanner(System.in).next();
-                        System.out.println("명령어을 입력하세요");
-                        command = new Scanner(System.in).next();
-                        Callretrofit.post_new_sensor(name, command,user_id);
-                        break;
-                    case "2":
-                        for (int i =0; i<sensors.size();i++){
-                            System.out.println(sensors.get(i).getIndex()+".센서명: "+sensors.get(i).getName()+", 명령어: "+sensors.get(i).getCommand());
-                        }
-                        System.out.println("수정할 센서의 인덱스를 선택하세요(skip: quit)");
-                        index_s=new Scanner(System.in).next();
-                        System.out.println("수정할 이름을 입략히세요(skip: quit)");
-                        name=new Scanner(System.in).next();
-                        System.out.println("수정할 명령어를 입력하세요(skip: quit)");
-                        command=new Scanner(System.in).next();
-                        if (index_s.equals("quit")|| name.equals("quit")||command.equals("quit")){
+                        if (name.equals("quit")){
                             break;
                         }
-                        Callretrofit.update_sensor(new Sensor(Integer.parseInt(index_s),name,command));
+                        System.out.println("명령어을 입력하세요(skip: quit)");
+                        command = new Scanner(System.in).next();
+                        if (command.equals("quit")){
+                            break;
+                        }
+                        factory_agent.sensors.append(name, command,user_id);
+                        break;
+                    case "2":
+                        factory_agent.sensors.print();
+                        System.out.println("수정할 센서의 인덱스를 선택하세요(skip: quit)");
+                        index_s=new Scanner(System.in).next();
+                        if (index_s.equals("quit")){
+                            break;
+                        }
+                        System.out.println("수정할 이름을 입략히세요(skip: quit)");
+                        name=new Scanner(System.in).next();
+                        if (name.equals("quit")){
+                            break;
+                        }
+                        System.out.println("수정할 명령어를 입력하세요(skip: quit)");
+                        command=new Scanner(System.in).next();
+                        if (command.equals("quit")){
+                            break;
+                        }
+                        factory_agent.sensors.update(new Sensor(Integer.parseInt(index_s),name,command));
                         break;
                     case "3":
-                        for (int i =0; i<sensors.size();i++){
-                            System.out.println(sensors.get(i).getIndex()+".센서명: "+sensors.get(i).getName()+", 명령어: "+sensors.get(i).getCommand());
-                        }
+                        factory_agent.sensors.print();
                         System.out.println("삭제할 센서의 인덱스를 선택하세요(skip: quit)");
                         index_s=new Scanner(System.in).next();
-                        Callretrofit.delete_sensor(sensors.get(Integer.parseInt(index_s)));
+                        factory_agent.sensors.delete(Integer.parseInt(index_s));
                 }
                 break;
             default:
@@ -167,16 +170,6 @@ public class Main {
             //  Handle any exceptions.
         }
     }
-    public static String get_dir(){
-        String dir;
-        if (System.getProperty("os.name").contains("Windows")) {
-            dir = "D:\\\\smartFactory\\sensors\\";
-        } else {
-            dir = "smartFactory/sensors/";
-        }
-        return dir;
-    }
-
 
 
 }
